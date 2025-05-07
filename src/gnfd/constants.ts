2b5d1adee52bcf5cb0de94d76ab2452df6bbcc25
robot-ux <robot.ux@protonmail.com>
2025-05-07 17:15:26 +0800

    fix: Update RPC URLs for testnet and mainnet in constants.ts

--- src/gnfd/constants.ts
+++ src/gnfd/constants.ts
@@ -1,9 +1,9 @@
 export const TESTNET = {
-  rpcUrl: "https://gnfd-testnet-fullnode-tendermint-ap.bnbchain.org",
+  rpcUrl: "https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org",
   chainid: 5600
 }
 
 export const MAINNET = {
-  rpcUrl: "https://greenfield-chain-ap.bnbchain.org",
+  rpcUrl: "https://greenfield-chain-us.bnbchain.org",
   chainid: 1017
 }
